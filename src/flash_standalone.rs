e26736ec9fd71ef18befa71c7bc0156590769465
Rob Pilling <robpilling@gmail.com>
2024-11-01 22:16:51 +0000

    flash algo: remove any loading from memory

--- src/flash_standalone.rs
+++ src/flash_standalone.rs
@@ -3,28 +3,45 @@
 
 type IpcFn = extern "C" fn(usize, usize, usize) -> !;
 
-const THUNK: *const [IpcFn; 4] = 0x20000000 as _;
+const P: usize = 0x20000000;
+//const THUNK: *const [IpcFn; 4] = P as _;
 
 #[link_section = ".text"]
 #[no_mangle]
 fn init(address: usize, clock_or_zero: usize, op: usize/*Operation*/) -> ! {
-    unsafe { (*THUNK)[0](address, clock_or_zero, op) }
+    unsafe {
+        let p = core::hint::black_box(P) + 4 * 0;
+        let p: IpcFn = core::mem::transmute(p);
+        p(address, clock_or_zero, op)
+    }
 }
 
 #[link_section = ".text"]
 #[no_mangle]
 fn uninit(op: usize/*Operation*/) -> ! {
-    unsafe { (*THUNK)[1](op, 0, 0) }
+    unsafe {
+        let p = core::hint::black_box(P) + 4 * 1;
+        let p: IpcFn = core::mem::transmute(p);
+        p(op, 0, 0)
+    }
 }
 
 #[link_section = ".text"]
 #[no_mangle]
 fn program_page(address: usize, byte_len: usize, buffer: *const u8) -> ! {
-    unsafe { (*THUNK)[2](address, byte_len, buffer as _) }
+    unsafe {
+        let p = core::hint::black_box(P) + 4 * 2;
+        let p: IpcFn = core::mem::transmute(p);
+        p(address, byte_len, buffer as _)
+    }
 }
 
 #[link_section = ".text"]
 #[no_mangle]
 fn erase_sector(address: usize) -> ! {
-    unsafe { (*THUNK)[3](address, 0, 0) }
+    unsafe {
+        let p = core::hint::black_box(P) + 4 * 3;
+        let p: IpcFn = core::mem::transmute(p);
+        p(address, 0, 0)
+    }
 }
